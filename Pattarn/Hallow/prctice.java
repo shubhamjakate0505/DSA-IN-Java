0d8013e42f6a333bf96c994fe1d66781cbc38fe9
Shubham Jakate <Shubhamjakate0505@gmail.com>
2024-07-03 18:16:30 +0530

    03/07/24  6:16

---
+++ Pattarn/Hallow/prctice.java
@@ -0,0 +1,36 @@
+/* import java.util.*;
+//hallow
+public class prctice{
+    public static void hall(int row,int col){
+        for(int i=1;i<=row;i++){// for the line
+            for(int j=1;j<=col;j++){//for the colm
+                if(i==1||i==row||j==1||j==col){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+       
+        hall(5,4);
+    }
+} */
+import java.util.*;
+public class prctice{
+    public static void overr(int row,int col){
+        int n=4;
+        for(int i=1;i<=row;i++){
+            for(j=1;j=n-i;j++){
+                System.out.print(" ")
+            }
+        }
+    }
+    
+    public static void main(String args[]){
+
+    }
+}
---
+++ Pattarn/Numberss/Desc.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Desc{
+    public static void inc(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+    }
+    
+    public static void main(String[] args){
+        inc(5);
+    }
+}
