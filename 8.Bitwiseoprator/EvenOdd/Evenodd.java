c45ddd407adc8971387b1323caf6be7e8efbc0a0
Shubham Jakate <Shubhamjakate0505@gmail.com>
2024-07-12 22:58:08 +0530

    12/07  10:58

---
+++ 8.Bitwiseoprator/EvenOdd/Evenodd.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Evenodd{
+    public static void Evenod(int n){
+        int bitmask=1;
+        if((n & bitmask)==0){
+            //even
+            System.out.println("Number is even:"+n);
+        
+        }else{
+            System.out.println("Number is odd:"+n);
+        }
+        
+    }
+    public static int getit(int n,int i){
+        int bitmaks=1<<i;
+        if((n&bitmaks)==0){
+            return 0;
+
+        }
+        else{
+            return 1;
+        }
+    }
+
+    
+    public static void main(String args[]){
+        System.out.println(getit(3,2));
+        // Evenod(11);
+        // Evenod(7);
+        // Evenod(12);
+        // Evenod(100);
+    }
+}
