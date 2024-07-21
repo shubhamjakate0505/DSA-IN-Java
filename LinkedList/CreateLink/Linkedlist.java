434535262425823c4b94921b76b90ae52c7330e3
Shubham Jakate <Shubhamjakate0505@gmail.com>
2024-07-21 21:10:46 +0530

    21/07/24 9:10

---
+++ LinkedList/CreateLink/Linkedlist.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Linkedlist{
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){//constractor
+            this.data=data;
+            this.next=null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    
+    public void addFrist(int data){
+        //step 1=create new node
+    }
+
+    public static void main(String args[]){
+        Linkedlist l1=new Linkedlist()
+
+    }
+}
---
+++ LinkedList/readme.md
@@ -0,0 +1,7 @@
+# LinkedList
+
+# Add Frist
+
+1. create new Node
+2. new node's next=head 
+3. Head=new Head
