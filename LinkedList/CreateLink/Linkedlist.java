a0582bbac772dd102be651d451ddcca087e3c15b
Shubham Jakate <Shubhamjakate0505@gmail.com>
2024-07-25 22:55:04 +0530

    25/07/24

--- LinkedList/CreateLink/Linkedlist.java
+++ LinkedList/CreateLink/Linkedlist.java
@@ -235,7 +235,18 @@ public class Linkedlist{
         }
         return true;
     }
-
+    public boolean isCycle(){
+        Node slow=head;//head pointer start with head
+        Node fast=head;
+        while(fast!=null && fast.next=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;//Cycle Exit
+            }
+        }
+        retun false;
+    }
     public static void main(String args[]){
         Linkedlist l1=new Linkedlist();
         
