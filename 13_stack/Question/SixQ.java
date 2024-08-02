4a93d2b4650e64f976674b8254fdeb3ce2a087c5
Shubham Jakate <Shubhamjakate0505@gmail.com>
2024-08-02 06:47:02 +0530

    02/08/24 6:46

---
+++ 13_stack/Question/SixQ.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class SixQ{
+    
+    public static boolean isvalid(String str){
+        Stack<Character> s=new Stack<>();
+
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);//charecter store kar liya
+            //OPening Condition
+            if(ch =='{' || ch=='(' || ch== '['){
+                s.push(ch);
+            }else{
+                //colosing case
+                if(s.isEmpty()){
+                    return false;
+                }
+                if((s.peek()=='(' && ch==')') ||(s.peek()=='{' && ch=='}')||(s.peek()=='[' && ch==']')){
+                    s.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        if(s.isEmpty()){
+            return true;
+
+        }else{
+            return false;
+        }
+    }
+    public static void main(String args[]){
+        String str="()";  //true
+        System.out.println(isvalid(str));
+         }
+}
