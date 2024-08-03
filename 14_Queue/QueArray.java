a057a46c708b1784bad60f4e4a5e7f05e8047c63
Shubham Jakate <Shubhamjakate0505@gmail.com>
2024-08-03 23:25:17 +0530

    03/08/24 11:25

---
+++ 14_Queue/QueArray.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class QueArray{
+    static class Queue{
+        static int arr[];
+        static int size;
+        static int rear;//starting Position
+        Queue(int n){
+            arr[]=new int[n];
+            size=n;
+            rear=-1;
+        }
+    }
+    public static boolean isEmpty(){
+        if(Queue==null){
+            return rear==-1;
+        }
+    }
+    //add
+    public static int add(int data){
+        if(rear==size-1){//Queue Full 
+            System.out.println("Queue Is Full");
+            return
+
+        }
+        rear=rear+1;
+        arr[rear]=data;
+    }
+    public static void main(String args[]){
+
+    }
+}
