1645736ca3f3662c39fa521606f95efef5014801
Shubham Jakate <Shubhamjakate0505@gmail.com>
2024-08-14 09:50:11 +0530

    14/08/24

---
+++ Binary_Search/PreorderTress.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class PreorderTress{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+    static class BinaryTree{
+        //
+        static int idx=-1;
+        public static Node buildTree(int nodes[]){//its return Node means Root Node
+        idx++;
+        if(nodes[idx]==-1){
+            return null;
+        }
+        Node newnode=new Node(nodes[idx]);
+        newnode.left=buildTree(nodes);
+        newnode.right=buildTree(nodes);
+        
+        return newnode;
+        }
+
+    }
+
+
+
+
+
+    public static void main(String args[]){
+        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree=new BinaryTree();
+        Node root=tree.buildTree(nodes);
+        System.out.println(root.data);
+    }
+}
